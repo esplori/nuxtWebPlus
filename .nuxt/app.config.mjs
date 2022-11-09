b4f75c0469102d2663e5a69462dd46c1d7b19065
wilkwo <2381773226@qq.com>
2022-11-09 22:47:54 +0800

    初始化项目

---
+++ .nuxt/app.config.mjs
@@ -0,0 +1,8 @@
+
+import { defuFn } from '/Users/wilkwo/qdd/workspace/nuxt-web-plus/node_modules/defu/dist/defu.mjs'
+
+const inlineConfig = {}
+
+
+
+export default defuFn(inlineConfig)
---
+++ .nuxt/dist/server/server.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///Users/wilkwo/qdd/workspace/nuxt-web-plus/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
