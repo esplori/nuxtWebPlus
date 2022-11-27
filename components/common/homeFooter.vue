3e9a941f7e562643bfb6c2b930eb2aeca1b4b0c1
2EN <2381773226@QQ.COM>
2022-11-27 18:29:04 +0800

    update footer

--- components/common/homeFooter.vue
+++ components/common/homeFooter.vue
@@ -9,11 +9,34 @@
   </div>
 </template>
 <script lang="ts" setup>
+import { onMounted } from "vue";
 useHead({
   script: [
-      { src: 'https://cpro.baidustatic.com/cpro/ui/cm.js' },
-      { src: 'https://source.dsiab.com/plugins/webstatistics.js' },
-    ]
+    { src: 'https://cpro.baidustatic.com/cpro/ui/cm.js' },
+    { src: 'https://source.dsiab.com/plugins/webstatistics.js' },
+  ]
+})
+
+const initWebStat = () => {
+  let webStats = new webStatistics({
+    baseUrl: "/bootService", // 基础接口地址url
+    url: "/stats/getStats.gif", // 请求上报api的接口地址
+    routeMode: "history", // 填写单页面应用中使用的路由模式。
+    autoUpload: true, // 是否自动请求接口，在setUserId之后会以baseUrl+url形式在页面切换时自动请求上报PV/UV的接口
+    prop: {
+      //请求参数映射，参数名默认如下，可以自定义修改参数名。
+      id: "visitorId",
+    },
+  });
+  setTimeout(() => {
+    webStats.setUserId();
+  }, 1000);
+}
+
+onMounted(() => {
+  if (process.client) {
+    initWebStat()
+  }
 })
 // export default {
 //   head() {
