2c25383df01f66c5f6ae59527bdc540f18f53473
2EN <2381773226@QQ.COM>
2022-12-01 00:09:56 +0800

    update api

--- plugins/baseUrl.ts
+++ plugins/baseUrl.ts
@@ -1,3 +1,4 @@
 // const baseUrl = 'http://admin.dsiab.com/bootService'
-const baseUrl = 'http://localhost:8091/bootService'
+const baseUrl = 'http://106.52.157.102:8081/portal-service'
+// const baseUrl = 'http://localhost:8082'
 export default baseUrl
