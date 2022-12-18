a9a6ca34421fdd4cca909c32ca33b817154b8d9a
2EN <2381773226@QQ.COM>
2022-12-18 20:01:58 +0800

    更新promote url

--- pages/tbk/api.js
+++ pages/tbk/api.js
@@ -1,5 +1,5 @@
-import preFix from '@/plugins/baseUrl'
+const  preFix  = 'http://106.52.157.102:8081/promote-service'
 
-export const getShopListApi = `${preFix}/tbk/getList/`
-export const getTbkDetailApi = `${preFix}/tbk/getDetail/`
-export const getRecommendApi = `${preFix}/tbk/getRecommend`
+export const getShopListApi = `${preFix}/promote/getList/`
+export const getTbkDetailApi = `${preFix}/promote/getDetail/`
+export const getRecommendApi = `${preFix}/promote/getRecommend`
