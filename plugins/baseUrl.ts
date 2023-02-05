add2bf4bc1932f36633d47b3abaa46e40f5a62f3
2EN <2381773226@QQ.COM>
2023-02-05 15:18:29 +0800

    更新请求地址

--- plugins/baseUrl.ts
+++ plugins/baseUrl.ts
@@ -1,4 +1,3 @@
-// const baseUrl = 'http://admin.dsiab.com/bootService'
-const baseUrl = 'http://106.52.157.102:8081/portal-service'
+const baseUrl = 'https://admin.dsiab.com/portal-service'
 // const baseUrl = 'http://localhost:8082'
 export default baseUrl
