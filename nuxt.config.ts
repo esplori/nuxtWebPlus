d79c1e719d89d5cb8451b9c503fef1fb9b1062f6
2EN <2381773226@QQ.COM>
2023-02-11 21:14:48 +0800

    update meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
-    // head: {
-    //   charset: 'utf-8',
-    //   viewport: 'width=500, initial-scale=1,width=device-width',
-    //   title: 'javascript技术分享',
-    //   meta: [
-    //     { name: 'description', content: '书写心得,分享感悟' },
-    //     { name: 'keywords', content: 'javascript技术分享,js技术,vuejs,web前端,前端开发,前端面试,web开发,dsiab,个人博客,前端博客' },
-    //   ],
-    // }
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=500, initial-scale=1,width=device-width',
+      meta: [
+        { name: 'description', content: '书写心得,分享感悟' },
+        { "http-equiv": 'content-language', content: "zh-cn" },
+        { name: 'keywords', content: 'javascript技术分享,js技术,vuejs,web前端,前端开发,前端面试,web开发,dsiab,个人博客,前端博客' },
+      ],
+      htmlAttrs: {'lang': "zh-cn"}
+    }
   },
   css: [
     "element-plus/dist/index.css",
