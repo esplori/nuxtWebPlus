c5d208361b981b5603cedbd45dc03cc0be589df9
wilkwo <2381773226@qq.com>
2025-06-07 13:04:50 +0800

    优化js加载顺序

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,16 +5,28 @@ export default defineNuxtConfig({
   app: {
     head: {
       charset: 'utf-8',
-      viewport: 'width=500, initial-scale=1,width=device-width',
+      viewport: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no',
       meta: [
         { name: 'description', content: '书写心得,分享感悟' },
         { "http-equiv": 'content-language', content: "zh-cn" },
+        { 'http-equiv': 'Content-Security-Policy', content: 'upgrade-insecure-requests' },
         { name: 'keywords', content: 'javascript技术分享,js技术,vuejs,web前端,前端开发,前端面试,web开发,dsiab,个人博客,前端博客' },
       ],
       script: [
         {
           src: "https://hm.baidu.com/hm.js?96ab5c4cd236e8e96687d2dab55e7034",
-          defer: true  // 添加defer属性延迟加载
+          async: true,  // 添加defer属性延迟加载
+          body: true, // 让js在body中加载 
+        },
+        {
+          src: "https://source.dsiab.com/plugins/webstatistics.js",
+          defer: true,
+          body: true,
+        },
+        {
+          src: "https://cpro.baidustatic.com/cpro/ui/cm.js",
+          defer: true,
+          body: true,
         }
       ],
       htmlAttrs: { 'lang': "zh-cn" }
@@ -22,7 +34,6 @@ export default defineNuxtConfig({
   },
   css: [
     "/assets/css/common.css",
-
   ],
   modules: [
     '@element-plus/nuxt'
