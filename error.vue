5d1b787ae60230d8d4f11634d86aeb64b51642de
wilkwo <2381773226@qq.com>
2025-07-14 10:26:05 +0800

    优化404跳转

--- error.vue
+++ error.vue
@@ -2,7 +2,7 @@
   <div class="not-found">
     <img src="@/assets/img/404.png" alt="" width="400px" style="margin:0 auto" />
     <div class="msg">抱歉，你访问的页面不存在</div>
-    <div class="seconds">{{ seconds }} 秒后将跳转到首页</div>
+    <div class="seconds">5秒后将跳转到首页</div>
   </div>
 </template>
 <script>
@@ -15,13 +15,9 @@ export default {
   mounted() {
     // 判断是否在客户端
     if (process.client) {
-      setInterval(() => {
-        this.seconds = this.seconds - 1;
-        if (this.seconds <= 0) {
-          this.seconds = 0
-          location.href = "/";
-        }
-      }, 1000);
+      setTimeout(() => {
+        location.href = "/";
+      }, 5000);
     }
   },
 };
