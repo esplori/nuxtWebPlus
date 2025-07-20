11653f265a1984a2301ccac44e5734a0dc73182a
wilkwo <2381773226@qq.com>
2025-07-20 10:03:02 +0800

    优化列表html去除规则

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -4,11 +4,12 @@
  * @returns 
  */
 export function delHtmlTag(str) {
-  if (process.client) {
-    // 浏览器端通过js原生方法去除Html标签
-    return (new DOMParser().parseFromString(str, "text/html").body.textContent || '')
-  } else {
-    return str
-  }
+  // if (process.client) {
+  //   // 浏览器端通过js原生方法去除Html标签
+  //   return (new DOMParser().parseFromString(str, "text/html").body.textContent || '')
+  // } else {
+  //   return str
+  // }
   // return str.replace(/[^(\u4e00-\u9fa5)。，,.]/g, "")
+  return str.replace(/<[^>]+>/g, "")
 }
